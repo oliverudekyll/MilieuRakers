24ef8b15a689cb8d593de7b22c7297af3dba49c2
oliverudekyll <oliverudekll@gmail.com>
2023-07-09 19:58:45 +0300

    commit11

--- Archive/archive.js
+++ Archive/archive.js
@@ -1,20 +1,62 @@
 // RISO
 
 var risoImagePaths = [
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-1.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-2.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-3.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-4.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-5.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-6.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-7.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-8.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-9.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-10.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-11.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-12.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-13.jpg",
-    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-14.jpg"
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-1.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-2.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-3.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-4.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-5.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-6.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-7.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-8.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-9.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-10.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-11.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-12.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-13.jpg"
+      text: "Scans from book"
+    },
+    {
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-14.jpg"
+      text: "Scans from book"
+    }
 ];
 
 var container = document.getElementById("feed");
@@ -24,7 +66,7 @@ for (var i = 0; i < risoImagePaths.length; i++) {
   div.className = "article";
 
   var img = document.createElement("img");
-  img.src = risoImagePaths[i];
+  img.src = risoImagePaths[i].path;
 
   if (i === 0) {
     div.id = "preface";
