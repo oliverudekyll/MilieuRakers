87027486ec871cc1976904dbcc5d63258879b11e
Oliver Udeküll <oliverudekll@gmail.com>
2023-07-09 10:55:30 +0300

    äkki siit

--- Archive/archive.js
+++ Archive/archive.js
@@ -1,20 +1,20 @@
 // RISO
 
 var risoImagePaths = [
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-1.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-2.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-3.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-4.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-5.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-6.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-7.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-8.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-9.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-10.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-11.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-12.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-13.jpg",
-    "//Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-14.jpg"
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-1.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-2.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-3.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-4.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-5.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-6.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-7.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-8.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-9.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-10.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-11.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-12.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-13.jpg",
+    "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-14.jpg"
 ];
 
 var container = document.getElementById("feed");
