e268dc75863ab522ababf1d90121ade6b4956951
Oliver Udeküll <oliverudekll@gmail.com>
2023-07-11 14:09:36 +0300

    digipildid

--- Archive/archive.js
+++ Archive/archive.js
@@ -2,76 +2,462 @@
 
 var risoImagePaths = [
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-1.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-1.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: "preface"
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-2.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-2.jpg",
+      textLeft: "Scans from book",
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-3.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-3.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-4.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-4.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-5.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-5.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-6.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-6.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-7.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-7.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-8.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-8.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-9.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-9.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: "grills"
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-10.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-10.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-11.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-11.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-12.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-12.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-13.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-13.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
     },
     {
-      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-14.jpg"
-      text: "Scans from book"
+      path: "/Archive/Images/Content/Riso/ErikMerisalu-Archive-Riso-14.jpg",
+      textLeft: ["Scans from book"],
+      textRight: ["textRight"],
+      id: ""
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-1.jpg",
+      textLeft: "Lucky charm",
+      textRight: ["textRight"],
+      id: ""
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-2.jpg",
+      textLeft: ["cell", "925 silver", "luca school of arts", "ghent"],
+      textRight: ["textRight"],
+      id: "exhibition-pieces"
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-3.jpg",
+      textLeft: "wearing cell",
+      textRight: ["textRight"],
+      id: ""
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-4.jpg",
+      textLeft: ["non-place", "scrap metal, bronze, crystal quartz", "luca school of arts", "ghent"],
+      textRight: ["textRight"],
+      id: ""
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-5.jpg",
+      textLeft: ["personal belongings", "925 silver", "luca school of arts", "ghent"],
+      textRight: ["textRight"],
+      id: ""
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-6.jpg",
+      textLeft: ["construction remnant bracelets", "luca school of arts", "ghent"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-7.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-8.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-9.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-10.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-11.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-12.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-13.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-14.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-15.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-16.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-17.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-18.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-19.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-20.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-21.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-22.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-23.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-24.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-25.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-26.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-27.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-28.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+
+        {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-29.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-30.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "face-jewels" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-31.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-32.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+
+        {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-33.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "earrings" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-34.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-35.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-36.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-37.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-38.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-39.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "bracelets" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-40.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-41.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "rings" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-42.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-43.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-44.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-45.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-46.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-47.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-48.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-49.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-50.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-51.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-52.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-53.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-54.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
+    },    
+    {
+      path: "/Archive/Images/Content/Digi/ErikMerisalu-Archive-Digi-55.jpg",
+      textLeft: ["textLeft"],
+      textRight: ["textRight"],
+      id: "" 
     }
 ];
 
-var container = document.getElementById("feed");
+var imgContainer = document.getElementById("feed");
 
 for (var i = 0; i < risoImagePaths.length; i++) {
-  var div = document.createElement("div");
-  div.className = "article";
+  var imgDiv = document.createElement("div");
+  imgDiv.className = "article";
+  imgDiv.id = risoImagePaths[i].id;
 
   var img = document.createElement("img");
   img.src = risoImagePaths[i].path;
 
-  if (i === 0) {
-    div.id = "preface";
-  }
+/*   if (i === 0) {
+    imgDiv.id = "preface";
+  } */
 
-  div.appendChild(img);
-  container.appendChild(div);
+  imgDiv.appendChild(img);
+  imgContainer.appendChild(imgDiv);
 }
+
+/* var targetImages = document.querySelectorAll("#feed .article img");
+
+var options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5 // Adjust this value as needed for the center crossing
+};
+
+var observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      var index = Array.from(targetImages).indexOf(entry.target);
+      var param = risoImagePaths[index].text;
+
+      if (param) {
+        p.textContent = param;
+      }
+    }
+  });
+}, options);
+
+targetImages.forEach(function (image) {
+  observer.observe(image);
+}); */
