3ca660d2aea9d6e552235589ecc587e500b834b5
Oliver Udeküll <oliverudekll@gmail.com>
2023-08-01 17:38:09 +0200

    test

--- js/works.js
+++ js/works.js
@@ -1,101 +1,114 @@
 var imagePaths = [
-    {
-        preview: "../Works/Images/1/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/2/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/3/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/4/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/5/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/6/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/7/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/8/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/9/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/10/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/11/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/12/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/13/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/14/Preview.jpg",
-        hover: []
-    },
-    {
-        preview: "../Works/Images/15/Preview.jpg",
-        hover: ["../Works/Images/15/1.jpg","../Works/Images/15/2.jpg","../Works/Images/15/3.jpg"]
-    },
-    {
-        preview: "../Works/Images/16/Preview.jpg",
-        hover: ["../Works/Images/16/1.jpg","../Works/Images/16/2.jpg"]
-    },
-    {
-        preview: "../Works/Images/17/Preview.jpg",
-        hover: ["../Works/Images/17/1.jpg"]
-    },
-    {
-        preview: "../Works/Images/18/Preview.jpg",
-        hover: ["../Works/Images/18/1.jpg","../Works/Images/18/2.jpg"]
-    },
-    {
-        preview: "../Works/Images/19/Preview.jpg",
-        hover: ["../Works/Images/19/1.jpg"]
-    },
-    {
-        preview: "../Works/Images/20/Preview.jpg",
-        hover: ["../Works/Images/20/1.jpg"]
-    },
-    {
-        preview: "../Works/Images/21/Preview.jpg",
-        hover: ["../Works/Images/21/1.jpg"]
-    },
-    {
-        preview: "../Works/Images/22/Preview.jpg",
-        hover: ["../Works/Images/22/1.jpg","../Works/Images/22/2.jpg"]
-    },
-    {
-        preview: "../Works/Images/23/Preview.jpg",
-        hover: ["../Works/Images/23/1.jpg"]
-    },
-    {
-        preview: "../Works/Images/24/Preview.jpg",
-        hover: ["../Works/Images/24/1.jpg"]
-    },
-]
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/27/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/27/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/27/2.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/27/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/26/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/26/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/26/2.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/26/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/25/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/25/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/25/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/24/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/24/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/24/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/23/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/23/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/23/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/22/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/22/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/22/2.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/22/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/21/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/21/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/21/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/20/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/20/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/20/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/19/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/19/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/19/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/18/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/18/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/18/2.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/18/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/17/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/17/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/17/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/16/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/16/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/16/2.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/16/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/15/Preview.jpg",
+    hover: ["https://oliverudekyll.github.io/milieurakers/Works/Images/15/1.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/15/2.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/15/3.jpg", "https://oliverudekyll.github.io/milieurakers/Works/Images/15/Preview.jpg"],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/14/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/13/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/12/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/11/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/10/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/9/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/8/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/7/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/6/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/5/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/4/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/3/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/2/Preview.jpg",
+    hover: [],
+  },
+  {
+    preview: "https://oliverudekyll.github.io/milieurakers/Works/Images/1/Preview.jpg",
+    hover: [],
+  }
+];
+
 
 var imgContainer = document.getElementById("feed");
 
@@ -105,18 +118,26 @@ for (var i = 0; i < imagePaths.length; i++) {
   
     var img = document.createElement("img");
     img.src = imagePaths[i].preview;
+    img.className = "works";
     imgDiv.id = "id-" + i;
+
+
   
     imgDiv.appendChild(img);
     imgContainer.appendChild(imgDiv);
   }
+
+  var emptyDiv = document.createElement("div");
+  emptyDiv.className = "empty";
+  imgContainer.appendChild(emptyDiv)
+
 
   var displace = function() {
     for (var i = 0; i < imagePaths.length; i++) {
         var image = document.getElementById("id-" + i);
 
         var randomTranslateX = Math.floor(Math.random() * 20) - 10;
-        var randomTranslateY = Math.floor(Math.random() * 50) - 25;
+        var randomTranslateY = Math.floor(Math.random() * 50) - 25; 
 
         if (image) {
             image.style.transform = `translate(${randomTranslateX}%, ${randomTranslateY}%)`;
@@ -136,7 +157,7 @@ function changeImageOnHover(index) {
 
     var hoverImages = imagePaths[index].hover;
     
-    // Check if the hoverImages array is not empty
+
     if (hoverImages.length > 0) {
       var currentImageIndex = 0;
 
@@ -145,20 +166,103 @@ function changeImageOnHover(index) {
         currentImageIndex = (currentImageIndex + 1) % hoverImages.length;
       }
 
-      // Set an interval to cycle through the hover images on hover
-      var interval = setInterval(cycleImages, 1000); // Change images every 500 milliseconds
+      var interval = setInterval(cycleImages, 750);
 
-      // Clear the interval when the mouse leaves the image
       image.addEventListener("mouseout", function () {
         clearInterval(interval);
-        // Revert to the default image when the mouse leaves the image
         image.src = imagePaths[index].preview;
       });
     }
   };
 }
 
-// Add event listeners to each image for hover effect
 for (var i = 0; i < imageElements.length; i++) {
   imageElements[i].addEventListener("mouseover", changeImageOnHover(i));
+}
+
+var logo = document.getElementById("logo");
+
+var logoUrls = [
+  "https://oliverudekyll.github.io/milieurakers/Archive/Images/Logos/ErikMerisalu-Website-Logo-1.png",
+  "https://oliverudekyll.github.io/milieurakers/Archive/Images/Logos/ErikMerisalu-Website-Logo-2.png"
+]
+
+var currentLogoIndex = 0;
+
+function changeLogo() {
+  logo.src = logoUrls[currentLogoIndex];
+  currentLogoIndex = (currentLogoIndex + 1) % logoUrls.length;
+}
+
+changeLogo();
+setInterval(changeLogo, 1000);
+
+function quickViewImage(index) {
+  var image = imageElements[index];
+  var hoverImages = imagePaths[index].hover;
+
+  if (hoverImages.length > 0) {
+    var quickViewOverlay = document.createElement("div");
+    quickViewOverlay.className = "quick-view-overlay";
+
+    var quickViewImage = document.createElement("img");
+    quickViewImage.src = hoverImages[0];
+    quickViewImage.className = "quick-view-image";
+
+    quickViewOverlay.appendChild(quickViewImage);
+    document.body.appendChild(quickViewOverlay);
+
+    var currentImageIndex = 1;
+
+    function cycleQuickViewImages() {
+      quickViewImage.src = hoverImages[currentImageIndex];
+      currentImageIndex = (currentImageIndex + 1) % hoverImages.length;
+    }
+
+    var interval = setInterval(cycleQuickViewImages, 750);
+
+    quickViewOverlay.addEventListener("click", function () {
+      clearInterval(interval);
+      document.body.removeChild(quickViewOverlay);
+    });
+  } else {
+
+    var quickViewOverlay = document.createElement("div");
+    quickViewOverlay.className = "quick-view-overlay";
+
+    var quickViewImage = document.createElement("img");
+    quickViewImage.src = imagePaths[index].preview;
+    quickViewImage.className = "quick-view-image";
+
+    quickViewOverlay.appendChild(quickViewImage);
+    document.body.appendChild(quickViewOverlay);
+
+    quickViewOverlay.addEventListener("click", function () {
+      document.body.removeChild(quickViewOverlay);
+    });
+  }
+}
+
+imgContainer.addEventListener("click", function (event) {
+  var clickedElement = event.target;
+  if (clickedElement.tagName === "IMG") {
+    var index = Array.from(imageElements).indexOf(clickedElement);
+    quickViewImage(index);
+  }
+});
+
+window.addEventListener("scroll", animateOnScroll);
+
+function animateOnScroll() {
+  var animateItems = document.querySelectorAll(".works");
+
+  animateItems.forEach(function(item) {
+    var itemTop = item.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    // Check if the item is in the viewport
+    if (itemTop < windowHeight) {
+      item.style.filter = "grayscale(0)";
+    }
+  });
 }
